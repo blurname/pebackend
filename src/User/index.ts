cc5426a41e8862ce315408216f555d00ee218efe
blurname <naughtybao@outlook.com>
2021-11-18 23:38:15 +0800

    feat: sign in/up

--- src/User/index.ts
+++ src/User/index.ts
@@ -9,23 +9,49 @@ type User = {
 }
 
 const signUp = async (user: User) => {
-	console.log(user)
+  const isExisted = await prisma.user.findFirst({
+    where: {
+      name: user.name,
+    },
+  })
+	if(isExisted !== null){
+		return null
+	}
+  const signedUser = await prisma.user.create({
+		data:{
+			name:user.name,
+			password:user.password
+		}
+  })
+  return signedUser
+}
+const signIn = async (user: User) => {
+	console.log('signin:',user)
   const signedUser = await prisma.user.findFirst({
     where: {
       name: user.name,
       password: user.password,
     },
   })
-  console.log(signedUser)
+	console.log('signedUser:', signedUser)
   return signedUser
 }
 
 user.get('/signin/?<name:string>&<password:string>').use(async (req) => {
+  const user = await signIn({
+    name: req.query.name,
+    password: req.query.password,
+  })
+	console.log(user)
+  return Response.json(user)
+})
+
+user.post('/signup/?<name:string>&<password:string>').use(async (req) => {
   const user = await signUp({
     name: req.query.name,
     password: req.query.password,
   })
-	console.log('here')
+	console.log(user)
   return Response.json(user)
 })
 
@@ -33,5 +59,6 @@ user.get('/info').use((req) => {
 	console.log('info')
 	return Response.text('info')
 })
+
 
 
